e442a925aa876f55aeab1f2f286d48c4e0d60244
BenAHawker <Benjamin.Hawker@outlook.com>
2021-07-09 13:17:15 -0400

    feature : (*) added tkinter start point --  Tkinter Start Point

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+
+# Create Window Object
+
+app = Tk()
+
+
+
+app.title('QR Generator')
+app.iconphoto(False,PhotoImage(file="./assets/img/icon.png"))
+app.geometry('700x350')
+
+# Start Program
+app.mainloop()
